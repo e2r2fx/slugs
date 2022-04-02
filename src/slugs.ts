3558f1fd1fa54a7b29aa7480e0b81f08fe4e3458
e2r2fx <e2r2fx>
2022-04-01 23:51:05 -0400

    basic slug logic

--- src/slugs.ts
+++ src/slugs.ts
@@ -1,1 +1,17 @@
-console.log('hey');
+import slugify from 'slugify';
+import fs from 'fs';
+import randomstring from 'randomstring';
+import data from './data.json';
+
+const slugs = data
+  .map((productName) => {
+    const slug = slugify(productName, { lower: true });
+    const id = randomstring.generate({
+      length: 5,
+      capitalization: 'lowercase',
+    });
+    return `${slug}-${id}`;
+  })
+  .join(`,\n`);
+
+fs.writeFileSync('./output.csv', slugs);
