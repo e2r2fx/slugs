09054a9dc2052afbeeb7e832df200fff1f19215a
e2r2fx <e2r2fx>
2022-04-13 17:17:02 -0400

    brickGen working js

--- src/brickGen.js
+++ src/brickGen.js
@@ -2,29 +2,116 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('./src/data/categories.json', 'utf-8');
-const brick = fs.readFileSync('./src/templates/brick.html', 'utf-8');
+const brickTemplate = fs.readFileSync('./src/templates/brick.html', 'utf-8');
 
 let categoryPaths = JSON.parse(data.toString());
 
 // Removed duplicated finalCategories
 categoryPaths = [...new Set(categoryPaths)];
-const result = [];
-const level = { result };
+const categoryTree = [];
+const level = { categoryTree };
 
 categoryPaths.forEach((categoryPath) => {
-  categoryPath.split('/').reduce((previousCategory, currentCategory) => {
-    if (!previousCategory[currentCategory]) {
-      // eslint-disable-next-line no-param-reassign
-      previousCategory[currentCategory] = { result: [] };
-      previousCategory.result.push({
-        name: currentCategory,
-        children: previousCategory[currentCategory].result,
-      });
-    }
-    return previousCategory[currentCategory];
-  }, level);
+  const categoryPathArray = categoryPath.split('/');
+  categoryPathArray.reduce(
+    (previousCategory, currentCategory, currentIndex) => {
+      if (!previousCategory[currentCategory]) {
+        // eslint-disable-next-line no-param-reassign
+        previousCategory[currentCategory] = { categoryTree: [] };
+
+        // generate category url
+        const url = categoryPathArray
+          .slice(0, currentIndex + 1)
+          .join('/')
+          .toLowerCase()
+          .replace(/\s+/g, '-')
+          .replace(/(default-category\/)/g, '');
+
+        previousCategory.categoryTree.push({
+          title: currentCategory,
+          level: currentIndex,
+          url,
+          children: previousCategory[currentCategory].categoryTree,
+        });
+      }
+      return previousCategory[currentCategory];
+    },
+    level
+  );
 });
 
-const finalBrick = brick.replaceAll(/{{hello}}/g, 'test');
+const generateTemplate = (category, accumulatedTemplate = '') => {
+  let template = '';
+  switch (category.level) {
+    case 0:
+      template = accumulatedTemplate;
+      break;
+    case 1:
+      template = `
+<div class='menu-item has-submenu'>
+<div class='menu-title'><a>${category.title}</a></div>
+  ${
+    category.children.length > 0
+      ? `
+  <div class='menu-content-wrap'>
+    <div class='menu-content'>
+      {{category2}}
+    </div>
+    <div class='menu-bottom-content'>
+        <div class='menu-bottom-item'><a href="{{store url='${category.url}'}}">Shop All ${category.title}</a></div>
+      </div>
+  </div>`
+      : ''
+  }
+</div>`;
+      break;
+    case 2:
+      template = `
+<div class='menu-content-item has-modile-submenu'>
+  <div class='menu-content-item-header'>${category.title}</div>
+    ${
+      category.children.length > 0
+        ? `
+    <div class='menu-content-item-submenu'>
+      <div class='submenu-item'>
+        <ol>
+          {{category3}}
+        </ol>
+      </div>
+    </div>`
+        : ''
+    }
+</div>`;
+      break;
+    case 3:
+      template = `
+<li><a href="{{store url='${category.url}'}}">${category.title}</a>
+  ${category.children.length > 0 ? `<ol>{{category4}}</ol>` : ''} 
+</li>`;
+      break;
+    case 4:
+      template = `
+<li><a href="{{store url='${category.url}'}}">${category.title}</a></li>`;
+      break;
+    default:
+      break;
+  }
+
+  template = accumulatedTemplate.replace(
+    `{{category${category.level}}}`,
+    `${template}{{category${category.level}}}`
+  );
+
+  if (category.children.length > 0) {
+    category.children.forEach((child) => {
+      template = `${generateTemplate(child, template)}`;
+    });
+    template.replace(`{{category${category.level}}}`, '');
+  }
+
+  return template.replace(`{{category${category.level + 1}}}`, '');
+};
+
+const finalBrick = generateTemplate(categoryTree[0], brickTemplate);
 
 fs.writeFileSync('./src/output/brick.html', finalBrick);
